5d0e866fce53e2fcb39e207f10931c3fbf7ad7f0
Kwonjaeseong <77simon@naver.com>
2024-07-23 16:32:09 +0900

    init

---
+++ js/swiper-slider.js
@@ -0,0 +1,32 @@
+// swiper slide
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// swiper2 slide
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
